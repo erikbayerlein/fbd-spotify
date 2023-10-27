6b0cebd09cfe7ebd6bd5e8ab15b32d07a6cb108c
erikbayerlein <erik.bayer@icloud.com>
2023-10-27 08:25:10 -0300

    created

---
+++ fdb-spotify-springboot/src/test/java/com/fbd/spotify/fdbspotifyspringboot/FdbSpotifySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbd.spotify.fdbspotifyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdbSpotifySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
